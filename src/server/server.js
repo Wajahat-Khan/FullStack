f27ff4a7aa7e512b1d42fa0bb281fc3a6ee485d8
Wajahat Hussain <wajahat.hussain@northbaysolutions.net>
2019-11-22 15:47:33 +0500

    EJs and Routing enabled and basic express section finalized

--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,24 @@
 import express from 'express'
 import config from '../config.js'
-import fs from 'fs'
+import fs, { readdirSync } from 'fs'
+import apiRouter from '../../api'
 
 const server=express()
 
+server.use(express.static('public'))
 server.get('/',(req,res)=>{
-  res.send("Welcome")
-})
+  res.render('index')
+});
 
-server.get('/about.html',(req,res)=>{
-  fs.readFile('./src/about.html',(err,data)=>{
-  
-    res.send(data.toString());
-  })
+server.set('view engine', 'ejs')
+// server.get('/about.html',(req,res)=>{
+//   fs.readFile('./src/about.html',(err,data)=>{
+    
+//     res.send(data.toString());
+//   })
 
-})
-
+// })
+server.use('/api',apiRouter);
 server.listen(config.port,()=>{
   console.log('listening to port', config.port);
 })
