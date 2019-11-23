95e0a26ace77ad32633eb0077664415271ebdeb5
Wajahat Hussain <wajahat.hussain@northbaysolutions.net>
2019-11-24 03:41:01 +0500

    api integration completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
+import axios from 'axios';
+
+
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state={pageHeader:"Naming Contest"}
+        this.state={pageHeader:"Naming Contest", contests:[]}
     }
-    componentDidMount(){}
+    componentDidMount(){
+      axios.get('/api/contests').then(
+        resp=> this.setState({contests:resp.data.contests})
+      ).catch(console.error)      
+    }
     componentWillUnmount(){}
     render(){
     return(
         <div className="App">  
           <Header message={this.state.pageHeader} />
         <div>
-          {this.props.contests.map(contest=>
-              <ContestPreview {...contest} />
+          {this.state.contests.map(contest=>
+              <ContestPreview key={contest.id} {...contest} />
           )}
         </div>
         </div>
