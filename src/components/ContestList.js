e9fc31a63a614f71dd3b87349c82b451438c1660
Wajahat Hussain <wajahat.hussain@northbaysolutions.net>
2019-11-26 00:56:38 +0500

    handle click implemented

--- src/components/ContestList.js
+++ src/components/ContestList.js
@@ -3,29 +3,19 @@ import ContestPreview from './ContestPreview';
 import PropTypes from 'prop-types';
 
 
+const ContestList = ({ contest }) => {
 
-class ContestList extends Component {
-    render() {
- 
-        return (
-            <div>
-                  {contest.map(element=>
-                      <ContestPreview key={element.id} {...element} />
-                  )}
-                </div>
-        );
-     
-    }
-}
-export default ContestList;
-const ContestList =({contest})=>{
-
-
+    return (
+        <div>
+            {contest.map(element =>
+                <ContestPreview key={element.id} {...element} />
+            )}
+        </div>
+    );
 
 }
-
 
 ContestList.propTypes = {
     contests: PropTypes.array
-  };
+};
 export default ContestList
--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const ContestPreview = (contest)=>{
-    return(
-        <div className="ContestPreview">
+
+
+class ContestPreview extends Component {
+    handleClick=()=>{
+        console.log(this.props.categoryName)
+    }
+    render() {
+        return (
+            <div className="ContestPreview" onClick={this.handleClick}>
             <div className="category-name">
-                {contest.categoryName}
+                {this.props.categoryName}
             </div>
             <div className="contest-name">
-                {contest.contestName}
+                {this.props.contestName}
             </div>
         </div>
-    )
+        );
+    }
+}
+
+
+ContestPreview.propTypes={
+categoryName: PropTypes.string.isRequired,
+contestName: PropTypes.string.isRequired
 }
 export default ContestPreview;
