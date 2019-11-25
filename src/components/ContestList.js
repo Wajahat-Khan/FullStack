ed418ddd46e455fdcda3dc9938ae44fc9e88a821
Wajahat Hussain <wajahat.hussain@northbaysolutions.net>
2019-11-25 20:47:56 +0500

    handle click functions

--- src/components/ContestList.js
+++ src/components/ContestList.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react';
 import ContestPreview from './ContestPreview';
 import PropTypes from 'prop-types';
 
+
+
+class ContestList extends Component {
+    render() {
+ 
+        return (
+            <div>
+                  {contest.map(element=>
+                      <ContestPreview key={element.id} {...element} />
+                  )}
+                </div>
+        );
+     
+    }
+}
+export default ContestList;
 const ContestList =({contest})=>{
-return (
-    <div>
-          {contest.map(element=>
-              <ContestPreview key={element.id} {...element} />
-          )}
-        </div>
-);
+
 
 
 }
